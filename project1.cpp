c2421a5003aa2009fe1d9b6e737481d36491605a
Allie Robbins <allierobbins@users.noreply.github.com>
2019-01-08 22:01:49 -0600

    Add files via upload

---
+++ project1.cpp
@@ -0,0 +1,34 @@
+/*
+ * project1.cpp
+ *
+ *  Created on: Aug 28, 2016
+ *      Author: Alexandria
+ */
+// Project 1 - Due 09.05.16
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Programming is great fun!" << endl;
+
+
+	cout << "I have taken C++ specific classes before while working on my Associates at SCC, " << endl;
+
+	cout << "but I hope to gain a clearer understanding of the language and its " << endl;
+
+	cout << "workings by the end of this class." << endl;
+	cout << "    " << endl;
+
+	cout << "Here is a dancing person!" << endl;
+	cout <<		" \\(._.)   " << endl;
+	cout <<		"    ))z  " << endl;
+	cout <<		"   / \\  " << endl;
+
+	cout << "They like to disco." << endl;
+	return 0;
+}
+
+
